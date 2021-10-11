356dda4facdf34a94d63c8a6d79281e48bae93da
Ozair Faisal <ozair.faisal@u.yale-nus.edu.sg>
2021-10-11 18:08:19 +0800

    Fixes

--- JustSlidin/app/src/main/java/com/swe/justSlidin/io/ClickAction.java
+++
@@ -1,8 +0,0 @@
-package com.swe.justslidin.io;
-
-
-import com.swe.justslidin.models.Position;
-
-public interface ClickAction extends Action {
-    void execute(Position a);
-}
--- JustSlidin/app/src/main/java/com/swe/justSlidin/models/Motion.java
+++
@@ -1,8 +0,0 @@
-package com.swe.justslidin.models;
-
-public class Motion extends Position {
-
-    public Motion(float x, float y) {
-        super(x, y);
-    }
-}
