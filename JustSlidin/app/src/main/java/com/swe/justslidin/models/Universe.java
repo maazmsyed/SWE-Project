fa28fdbf122394bd93238f2448887f5a696ebed0
superdanny68 <57492616+superdanny68@users.noreply.github.com>
2021-10-17 18:00:30 +0800

    Edit Universe

--- JustSlidin/app/src/main/java/com/swe/justslidin/models/Universe.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/models/Universe.java
@@ -1,42 +1,61 @@
 package com.swe.justslidin.models;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
 
 public class Universe {
 
     Motion gravity;
-    List<Character> data;
+    // List<Character> data;
+    List<Elements> elements;
 
     public Universe () {
-        this (new Motion(0,5f)); // TODO : constant is bad !!
+        this (new Motion(0,-5f)); // TODO : constant is bad !!
     }
 
     public Universe (Motion g) {
-        data = new Vector<>();
+        elements = new Vector<>();
         gravity = g;
     }
 
-    public void addChar(float x, float y) {
-        data.add(new Character (x,y));
+//    public void addChar(float x, float y, float rad) {
+//        data.add(new Character (x,y,rad));
+//        castChanges();
+//    }
+
+    public void addCoin(float x, float y, float rad) {
+        elements.add(new Coin(x, y, rad));
         castChanges();
     }
 
-    public Collection<Character> getChar() {
-        return data;
+    public void addBarrier(float x, float y, float h, float l) {
+        elements.add(new Barrier (x,y,h,l));
+        castChanges();
+    }
+
+    public List<Elements> getElements() {
+        return elements;
     }
 
     public void step() {
-        for (Character b : data) {
-            b.move(this.gravity);
+        for (Elements e : elements) {
+            e.moveUp(this.gravity);
         }
         castChanges();
     }
 
-    public void addChar(Position pos) {
-        this.addChar(pos.getX(),pos.getY());
+    public void addCoin(Position pos, float rad) {
+        this.addCoin(pos.getX(),pos.getY(),rad);
     }
+
+    public void addBarrier(Position pos, float h, float l) {
+        this.addBarrier(pos.getX(), pos.getY(),h,l);
+    }
+
+
+//    public void addChar(Position pos) {
+//        this.addChar(pos.getX(),pos.getY());
+//    }
 
     public interface Callback {
         void universeChanged ( Universe u ) ;
