6d9354abd6bf6a5e62fd3574d1f7157e970d52bb
maazmsyed <syedmmaaz@gmail.com>
2021-10-23 23:21:31 +0800

    Unit test changes

--- JustSlidin/app/src/test/java/com/swe/justslidin/TestUniverse.java
+++ JustSlidin/app/src/test/java/com/swe/justslidin/TestUniverse.java
@@ -12,13 +12,12 @@ public class TestUniverse {
     public void test_add_and_step() {
         //TODO: TEST DOESN'T WORK
         Universe u = new Universe();
+        //System.out.println(u.getPosition())
+        u.moveCharLeft(10f);
+        //System.out.println(b.toString());
         //System.out.println(u.getPosition());
-        Character b = new Character(540, 100, 50f);
-        b.moveLeft(10f);
-        System.out.println(b.toString());
-        //System.out.println(u.getPosition());
-        b.moveRight(10f);
-        System.out.println(b.toString());
+        u.moveCharRight(10f);
+        //System.out.println(b.toString());
         //System.out.println(u.getPosition());
         u.step();
         }
