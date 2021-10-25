c7d95c05a6f9c8bb121646bf82214fa972c8b6c3
Mustafa <syedmustafa@u.yale-nus.edu.sg>
2021-10-26 00:05:24 +0800

    Added constants file

---
+++ JustSlidin/app/src/main/java/com/swe/justslidin/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.swe.justslidin.constants;
+
+import com.swe.justslidin.models.Motion;
+
+public abstract class Constants {
+
+    public static final Motion DEFAULT_GRAVITY_MOTION = new Motion(0,5f);
+
+
+
+
+}
