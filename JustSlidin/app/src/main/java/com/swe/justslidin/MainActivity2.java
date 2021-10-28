a422e9039384918e41fea2ca9245429d1616c89b
maazmsyed <syedmmaaz@gmail.com>
2021-10-29 02:17:49 +0800

    More Firebase fixes

--- JustSlidin/app/src/main/java/com/swe/justslidin/MainActivity2.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/MainActivity2.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        
+        
     }
 }
