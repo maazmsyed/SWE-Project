7fe99991baae20e8f610294ebb945f1b512fadba
superdanny68 <57492616+superdanny68@users.noreply.github.com>
2021-10-29 02:30:16 +0800

    Adding test activity for database connection

--- JustSlidin/app/src/main/java/com/swe/justslidin/MainActivity2.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/MainActivity2.java
@@ -1,11 +1,18 @@
 package com.swe.justslidin;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -15,7 +22,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+                _myRef.setValue(dtf.format(now));
+            }
+        });
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         _myRef=database.getReference("message");
         
