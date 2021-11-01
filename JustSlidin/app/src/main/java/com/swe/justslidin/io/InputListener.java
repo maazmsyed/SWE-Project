87c7966b9dafb16fc077ccc82366163662576b77
Mustafa <syedmustafa@u.yale-nus.edu.sg>
2021-11-01 22:22:43 +0800

    Example MotionEvent.ACTION_DOWN

--- JustSlidin/app/src/main/java/com/swe/justslidin/io/InputListener.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/io/InputListener.java
@@ -17,6 +17,8 @@ public class InputListener implements View.OnTouchListener {
     private Position
             up_pos;
     private Callback callback;
+//    private Boolean holding;
+
     public void setCallback(InputHandler cb) {
         this.callback = cb;
     }
@@ -26,8 +28,19 @@ public class InputListener implements View.OnTouchListener {
         switch(motionEvent.getAction())
         {
             case MotionEvent.ACTION_DOWN:
+//                int temp = 0;
+//                this.holding = true;
+//                while (this.holding) {
+//                    if (temp % 5 == 0) {
+//                        // Keep on moving player
+//                        up_pos = new Position(motionEvent.getRawX(), motionEvent.getRawY());
+//                        callback.onClick(up_pos);
+//                    }
+//                    temp += 1;
+//                }
                 break;
             case MotionEvent.ACTION_UP:
+//                this.holding = false;
                 up_pos = new Position(motionEvent.getRawX(), motionEvent.getRawY());
                 callback.onClick(up_pos);
                 break;
