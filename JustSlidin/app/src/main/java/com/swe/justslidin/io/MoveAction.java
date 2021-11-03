58dad40f72fbded290161cd1d8d85afdec11ce48
Ozair Faisal <ozair.faisal@u.yale-nus.edu.sg>
2021-11-03 18:25:24 +0800

    Update player pos to database

--- JustSlidin/app/src/main/java/com/swe/justslidin/io/MoveAction.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/io/MoveAction.java
@@ -1,5 +1,6 @@
 package com.swe.justslidin.io;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.swe.justslidin.App;
 import com.swe.justslidin.constants.Constants;
 import com.swe.justslidin.models.Position;
 import com.swe.justslidin.models.Universe;
@@ -14,15 +16,15 @@ import com.swe.justslidin.models.Universe;
 
 
 
-public class MoveAction implements ClickAction { //TODO: extends AppCompatActivity
+public class MoveAction implements ClickAction { // TODO: extends AppCompactActivity
     private static final String TAG = "MoveAction";
     private final Universe universe;
     private final float screenWidth = Constants.SCREEN_WIDTH;
-    String playerName = "";
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance("https://justslidin-d4b80-default-rtdb.asia-southeast1.firebasedatabase.app/");
-    //TODO: SharedPreferences preferences = getSharedPreferences("PREFS",0);
-    DatabaseReference move_ref= database.getReference("players/" + playerName).child("pos").child("x");
+    public Context context = App.getInstance();
+    public SharedPreferences preferences = context.getSharedPreferences("PREFS",0);
+    private String playerName = preferences.getString("playerName", "");
+    private DatabaseReference playerRef;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance("https://justslidin-d4b80-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
 
 
@@ -30,30 +32,28 @@ public class MoveAction implements ClickAction { //TODO: extends AppCompatActivi
         this.universe = universe;
     }
 
-    // db declaration
-    // sp declaration
-    // get the player from sp
 
     @Override
     public void execute(Position pos) {
         Log.i(TAG, "MoveAction executed");
+        Log.i(TAG, playerName);
         //universe.addChar(pos);
-
-        //TODO: playerName = preferences.getString("playerName","");
+        this.playerRef = database.getReference("players/"+playerName);
 
         if (pos.getX() <= screenWidth / 2){
             Log.i(TAG,"ON THE LEFT");
             // universe.moveCharLeft(10f);
             this.universe.getPlayer().moveLeft(15);     // TODO; Check with Maaz
-            Log.i(TAG, move_ref.toString());
-            move_ref.setValue(this.universe.getPlayer().getPosition().getX());
+            this.playerRef.child("pos").setValue(this.universe.getPlayer().getPosition());
+            Log.i(TAG,"ON THE LEFT TO THE DB");
 
         }
         else{
             Log.i(TAG,"ON THE RIGHT");
             // universe.moveCharRight(10f);
             this.universe.getPlayer().moveRight(15);
-            move_ref.setValue(this.universe.getPlayer().getPosition().getX());
+            this.playerRef.child("pos").setValue(this.universe.getPlayer().getPosition());
+            Log.i(TAG,"ON THE RIGHT TO THE DB");
         }
 
     }
