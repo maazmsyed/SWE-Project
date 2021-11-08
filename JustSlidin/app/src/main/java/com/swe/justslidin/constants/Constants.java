764f44602438acec18448ba4b1362b7127773e5a
Ozair Faisal <ozair.faisal@u.yale-nus.edu.sg>
2021-11-09 02:12:05 +0800

    Updated constants

--- JustSlidin/app/src/main/java/com/swe/justslidin/constants/Constants.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/constants/Constants.java
@@ -1,26 +1,28 @@
 package com.swe.justslidin.constants;
 
 import android.content.res.Resources;
-import com.swe.justslidin.models.Motion;
 
 
 public class Constants {
 
-    public float SCREEN_WIDTH; // = Resources.getSystem().getDisplayMetrics().widthPixels;
-    public float SCREEN_HEIGHT; // = Resources.getSystem().getDisplayMetrics().heightPixels;
+    public float SCREEN_WIDTH;
+    public float SCREEN_HEIGHT;
 
     // Coin
-    public float COIN_RADIUS; // = SCREEN_HEIGHT / 40f;
+    public float COIN_RADIUS;
 
     // Barrier
-    public float BARRIER_SHORT_SIZE; // = SCREEN_WIDTH / 5f;
-    public float BARRIER_LONG_SIZE; // = SCREEN_WIDTH / 3f;
-    public float BARRIER_HEIGHT; // = SCREEN_HEIGHT / 18f;
+    public float BARRIER_SHORT_SIZE;
+    public float BARRIER_LONG_SIZE;
+    public float BARRIER_HEIGHT;
 
     // Player
-    public float PLAYER_RADIUS; // = SCREEN_WIDTH / 20f;
+    public float PLAYER_RADIUS;
+    public float PLAYER_MOVE_LEFT;
+    public float PLAYER_MOVE_RIGHT;
+    public float PLAYER_GRAVITY;
 
-    public Constants() {
+    public Constants(){
 
         try {
             SCREEN_HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels;
@@ -40,6 +42,9 @@ public class Constants {
 
         // Player
         PLAYER_RADIUS = SCREEN_WIDTH / 20f;
+        PLAYER_MOVE_LEFT = SCREEN_WIDTH / 25f;
+        PLAYER_MOVE_RIGHT = SCREEN_WIDTH / 25f;
+        PLAYER_GRAVITY = 10f;
 
     }
 
