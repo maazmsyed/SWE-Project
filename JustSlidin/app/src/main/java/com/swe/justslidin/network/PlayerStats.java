475cf93ef8a24694ee033af55d9e01f10f80bce6
maazmsyed <syedmmaaz@gmail.com>
2021-11-09 01:38:10 +0800

    PlayerStats data

--- JustSlidin/app/src/main/java/com/swe/justslidin/network/PlayerStats.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/network/PlayerStats.java
@@ -1,4 +1,17 @@
 package com.swe.justslidin.network;
 
 public class PlayerStats {
+
+    public static String playerID = "";
+    public static String otherPlayerID = "";
+    public static long elapsedTime;
+    public static int coinCounter;
+    public static int otherCoinCounter;
+    public static long otherElapsedTime;
+    public static float otherPlayerScreenHeight = 1794;
+    public static float otherPlayerScreenWidth = 1080;
+    public static Boolean gameEnded = false;
+
+
+//    long elapsedTime = System.currentTimeMillis() - startTime;
 }
