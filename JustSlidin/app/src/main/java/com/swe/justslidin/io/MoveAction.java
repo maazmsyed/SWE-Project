37951e6977c3f34be5a5ca23edad45e888b3a672
superdanny68 <57492616+superdanny68@users.noreply.github.com>
2021-11-09 15:33:53 +0800

    added MoveAction documentation

--- JustSlidin/app/src/main/java/com/swe/justslidin/io/MoveAction.java
+++ JustSlidin/app/src/main/java/com/swe/justslidin/io/MoveAction.java
@@ -7,6 +7,12 @@ import com.swe.justslidin.constants.Constants;
 import com.swe.justslidin.models.Position;
 import com.swe.justslidin.models.Universe;
 
+/**
+ * The MoveAction class implements ClickAction (which itself extends Action)
+ * Here, the position of the character (within the given Universe) will be updated according to a user's clicking position
+ * The .execute() function, given the user's clicking coordinates, then performs the updating of the user's positions in the universe
+ */
+
 public class MoveAction implements ClickAction {
     private static final String TAG = "MoveAction";
     private final Universe universe;
@@ -17,6 +23,15 @@ public class MoveAction implements ClickAction {
     public MoveAction(Universe universe) {
         this.universe = universe;
     }
+
+    /**
+     * The .execute function that ClickActions possess
+     * @param pos is the Position object parameter that this function takes
+     * There are two possible scenarios in this case: one where the clicking position is on the left half of the screen, and the other the right half
+     * We have a constant screenWidth that documents the width of the screen. For standard Pixel 2s, it is 540
+     * To move the characters, we first get the given universe (this.universe), then the character (.getPlayer), then call the moveRight/moveLeft functions
+     * we specify the child nodes "players" -> "pos" (documents position of that specific player)
+     */
 
     @Override
     public void execute(Position pos) {
